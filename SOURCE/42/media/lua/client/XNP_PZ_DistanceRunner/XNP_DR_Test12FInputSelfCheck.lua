aa76841bf78e1d86c451976357a5c9b0f8548260
XNPHL <225188554+XN-PHL@users.noreply.github.com>
2026-08-03 17:34:59 +0800

    Publish 2.3.0-test.1 multi-record inheritance source

---
+++ SOURCE/42/media/lua/client/XNP_PZ_DistanceRunner/XNP_DR_Test12FInputSelfCheck.lua
@@ -0,0 +1,14 @@
+XNP_PZ_DistanceRunner = XNP_PZ_DistanceRunner or {}
+
+local Core = XNP_PZ_DistanceRunner
+if not Core.test12f_input_self_check_logged then
+    Core.test12f_input_self_check_logged = true
+    print("[XNP TEST12F INPUT SELF CHECK]"
+        .. " build_marker=XNP_V2_230_TEST1_MULTI_RECORD_INHERITANCE_A"
+        .. " single_click_direct_path_count=0"
+        .. " double_click_confirmed_path_count=1"
+        .. " core_ticket_gate=true"
+        .. " double_click_window_ms=500")
+end
+
+return true
