b808fd5c42565e5efcc667869d3f20da76dad54f
XNPHL <225188554+XN-PHL@users.noreply.github.com>
2026-08-03 17:34:43 +0800

    Publish 2.3.0-test.1 multi-record inheritance source

--- SOURCE/42/media/lua/client/XNP_PZ_DistanceRunner/XNP_DR_PurplePhoenixDamageGuard.lua
+++ SOURCE/42/media/lua/client/XNP_PZ_DistanceRunner/XNP_DR_PurplePhoenixDamageGuard.lua
@@ -1,3 +1,10 @@
+local XNPChannelGuard = require "XNP_PZ_DistanceRunner/XNP_DR_ChannelGuard"
+if type(XNPChannelGuard) == "table"
+    and type(XNPChannelGuard.allowRuntime) == "function"
+    and not XNPChannelGuard.allowRuntime() then
+    return
+end
+
 require "XNP_PZ_DistanceRunner/XNP_DR_PhoenixTransaction"
 require "XNP_PZ_DistanceRunner/XNP_DR_PurplePhoenixInvulnerability"
 require "XNP_PZ_DistanceRunner/XNP_DR_CanonicalPlayerIdentity"
